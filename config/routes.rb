223d3bdb2002c7f634cd8c0e72d9ac0a329c91e6
serafimd <serafimd@serafimd-desktop>
2016-10-20 18:06:52 +0300

    Добавление аутентификации к главным контроллерам. Мелкие фиксы

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :protocols
+  resources :customers
   # get 'sessions/new'
   # get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -7,6 +9,7 @@ Rails.application.routes.draw do
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
   root :to => "users#new"
+  #root :to => "protocols#index"
   resources :users
   resources :sessions
 
